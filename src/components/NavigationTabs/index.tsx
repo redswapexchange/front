8c2c40149b17aac34736e72933d47245f3ae7043
sid <sid@better-verse.com>
2022-10-10 09:25:31 +0800

    Import Pool

--- src/components/NavigationTabs/index.tsx
+++ src/components/NavigationTabs/index.tsx
@@ -140,7 +140,7 @@ export function FindPoolTabs() {
           <HistoryLink to="/pool">
             <StyledArrowLeft />
           </HistoryLink>
-          <ActiveText>Import Pool</ActiveText>
+          <ActiveText>Pool</ActiveText>
           <QuestionHelper text={"Use this tool to find pairs that don't automatically appear in the interface."} />
         </RowBetween>
       </Tabs>
